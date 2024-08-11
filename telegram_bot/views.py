99fa9a6ce11e993ea2a373cc385eaeb87ddadb0f
Maksym <mks.viunov@gmail.com>
2024-08-11 14:42:39 +0200

    API vs Heroku integration, try to catch error

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -10,29 +10,32 @@ def telegram_webhook(request):
 
     if request.method == "POST":
         try:
-            data = json.loads(request)
-            # Przetwarzaj dane z Telegrama tutaj
-            print(data)  # Debugowanie: sprawdź, co przychodzi z Telegrama
+            data = json.loads(request.body)
+            print(data)
             return JsonResponse({"status": "ok"}, status=200)
-        except Exception as e:
-            context = {
-                'message': f"{e}"
-            }
-            return render(request, 'index.html', context)
-    elif request.method == 'GET':
-        context = {
-        'message': '!!!GET'
-    }
-        return render(request, 'index.html', context)
-            # print(f"Błąd: {e}!!!")  # Logowanie błędu
-            # return JsonResponse({"status": "error"}, status=500)
-    # return JsonResponse({"status": "bad request"}, status=400)
-    else:
-        context = {
-        'message': '!'
-    }
-    # print("ok")
-    return render(request, 'index.html', context)
+        except json.JSONDecodeError as e:
+            print(f"Decode json error: {e}")
+            return JsonResponse({
+                                "satatus": "error", 
+                                "message": "Invalid JSON"
+                                }, status=400)
+        except Exeption as e:
+            print (f"Unexpected error: {e}")
+            return JsonResponse({
+                                "satatus": "error", 
+                                "message": "Internal Server Error"
+                                }, status=500)
+    return JsonResponse({"status": "bad request"}, status=400)
+    # elif request.method == 'GET':
+    #     context = {
+    #     'message': '!!!GET'
+    # }
+    #     return render(request, 'index.html', context)
+    # else:
+    #     context = {
+    #     'message': '!'
+    # }
+    # return render(request, 'index.html', context)
 
 def my_page_view(request):
     # Możesz przekazać zmienne do szablonu, korzystając z kontekstu
