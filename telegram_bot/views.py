8a70be6642b57e4e433aeb22471628f34571cfef
Maksym <mks.viunov@gmail.com>
2024-08-12 18:32:55 +0200

    Deploy openai model 1

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -5,27 +5,27 @@ from django.http import HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-@csrf_exempt
-def telegram_webhook(request):
+# @csrf_exempt
+# def telegram_webhook(request):
 
-    if request.method == "POST":
-        try:
-            data = json.loads(request.body)
-            print(data)
-            return JsonResponse({"status": "ok"}, status=200)
-        except json.JSONDecodeError as e:
-            print(f"Decode json error: {e}")
-            return JsonResponse({
-                                "satatus": "error", 
-                                "message": "Invalid JSON"
-                                }, status=400)
-        except Exeption as e:
-            print (f"Unexpected error: {e}")
-            return JsonResponse({
-                                "satatus": "error", 
-                                "message": "Internal Server Error"
-                                }, status=500)
-    return JsonResponse({"status": "bad request"}, status=400)
+#     if request.method == "POST":
+#         try:
+#             data = json.loads(request.body)
+#             print(data)
+#             return JsonResponse({"status": "ok"}, status=200)
+#         except json.JSONDecodeError as e:
+#             print(f"Decode json error: {e}")
+#             return JsonResponse({
+#                                 "satatus": "error", 
+#                                 "message": "Invalid JSON"
+#                                 }, status=400)
+#         except Exeption as e:
+#             print (f"Unexpected error: {e}")
+#             return JsonResponse({
+#                                 "satatus": "error", 
+#                                 "message": "Internal Server Error"
+#                                 }, status=500)
+#     return JsonResponse({"status": "bad request"}, status=400)
     # elif request.method == 'GET':
     #     context = {
     #     'message': '!!!GET'
@@ -37,10 +37,10 @@ def telegram_webhook(request):
     # }
     # return render(request, 'index.html', context)
 
-def my_page_view(request):
-    # Możesz przekazać zmienne do szablonu, korzystając z kontekstu
-    context = {
-        'message': 'Witamy na naszej stronie!'
-    }
-    print("ok")
-    return render(request, 'index.html', context)
+# def my_page_view(request):
+#     # Możesz przekazać zmienne do szablonu, korzystając z kontekstu
+#     context = {
+#         'message': 'Witamy na naszej stronie!'
+#     }
+#     print("ok")
+#     return render(request, 'index.html', context)
