97e46e84c1b4b3af9fa8bef663fed0b6a42e5160
Maksym <mks.viunov@gmail.com>
2024-09-15 04:54:13 +0200

    drive auth changes

--- app_utils/drive_auth.py
+++ app_utils/drive_auth.py
@@ -4,18 +4,17 @@ from google.auth.transport.requests import Request
 import os
 import pickle
 
-# Scoopes define the level of accesss the app has to Google Drive
+# Scopes define the level of access the app has to Google Drive
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def authenticate_drive():
+def authenticate_drive(credentials_path, token_path='token.pickle'):
     """
     Authenticate and return the Google Drive service object.
     This function handles OAuth 2.0 authentication flow and manages credentials.
     """
     creds = None
-    token_path = 'credentials/token.pickle'
 
-    # Check if tocken.pickle exists to avoid re-authentication
+    # Check if token.pickle exists to avoid re-authentication
     if os.path.exists(token_path):
         with open(token_path, 'rb') as token:
             creds = pickle.load(token)
@@ -25,11 +24,11 @@ def authenticate_drive():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            # Run OAuth flow for the first time
+            # Run OAuth flow for the first time using the credentials_path
             flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
 
-        # Save the credentials for future use
+        # Save the credentials for future use in token.pickle
         with open(token_path, 'wb') as token:
             pickle.dump(creds, token)
 
